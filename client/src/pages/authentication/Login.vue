067eb7963918f739cf24a5e65aba42df11b7178e
a.konuhov <alexandrkonuhov@gmail.com>
2018-09-27 09:28:26 +0300

    first commit

---
+++ client/src/pages/authentication/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="authentication-wrapper authentication-3">
+    <div class="authentication-inner">
+
+      <!-- Side container -->
+      <!-- Do not display the container on extra small, small and medium screens -->
+      <div class="d-none d-lg-flex col-lg-8 align-items-center ui-bg-cover ui-bg-overlay-container p-5" style="background-image: url('/static/img/bg/21.jpg');">
+        <div class="ui-bg-overlay bg-dark opacity-50"></div>
+
+        <!-- Text -->
+        <div class="w-100 text-white px-5">
+          <h1 class="display-2 font-weight-bolder mb-4">НЕКОММЕРЧЕСКАЯ<br>ОРГАНИЗАЦИЯ</h1>
+          <div class="text-large font-weight-light">
+            КРЫМСКИЙ РЕСПУБЛИКАНСКИЙ ФОНД РАЗВИТИЯ ЖИЛИЩНОГО СТРОИТЕЛЬСТВА И ИПОТЕЧНОГО КРЕДИТОВАНИЯ
+          </div>
+        </div>
+        <!-- /.Text -->
+      </div>
+      <!-- / Side container -->
+
+      <!-- Form container -->
+      <div class="d-flex col-lg-4 align-items-center bg-white p-5">
+        <!-- Inner container -->
+        <!-- Have to add `.d-flex` to control width via `.col-*` classes -->
+        <div class="d-flex col-sm-7 col-md-5 col-lg-12 px-0 px-xl-4 mx-auto">
+          <div class="w-100">
+
+            <!-- Logo -->
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="ui-w-120">
+                <div class="w-100 position-relative">
+                  <img src="/static/img/logo.png" alt="logo" class="img-fluid">
+                </div>
+              </div>
+            </div>
+            <!-- / Logo -->
+
+            <h4 class="text-center text-lighter font-weight-normal mt-5 mb-0">АИС ипотечного кредитования</h4>
+
+            <!-- Form -->
+            <form class="my-5">
+              <b-form-group label="Логин">
+                <b-input v-model="credentials.email" />
+              </b-form-group>
+              <b-form-group>
+                <div slot="label" class="d-flex justify-content-between align-items-end">
+                  <div>Пароль</div>
+                  <a href="javascript:void(0)" class="d-block small">Забыли пароль?</a>
+                </div>
+                <b-input type="password" v-model="credentials.password" />
+              </b-form-group>
+
+              <div class="d-flex justify-content-between align-items-center m-0">
+                <!-- <b-check v-model="credentials.rememberMe" class="m-0">Запомнить</b-check> -->
+                <b-btn variant="primary" @click="onSubmitLogin">Войти в систему</b-btn>
+              </div>
+            </form>
+            <!-- / Form -->
+
+          </div>
+        </div>
+      </div>
+      <!-- / Form container -->
+
+    </div>
+  </div>
+</template>
+
+<!-- Page -->
+<style src="@/vendor/styles/pages/authentication.scss" lang="scss"></style>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'pages-authentication-login',
+  metaInfo: {
+    title: 'Login'
+  },
+  data: () => ({
+    credentials: {
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    onSubmitLogin () {
+      axios.post('http://localhost:3000/api/users/login', this.credentials)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
---
+++ client/src/pages/main/Index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <layout-flex>
+    <h1>asasas</h1>
+  </layout-flex>
+</template>
+
+<script>
+import Layout2Flex from '@/layout/Layout2Flex'
+export default {
+  name: 'PageMain',
+  components: {
+    'layout-flex': Layout2Flex
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
