85d6387d35ac6b71b929744e54baec53495e1117
a.konuhov <alexandrkonuhov@gmail.com>
2018-11-05 15:28:51 +0500

    sas

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -16,8 +16,8 @@ Axios.defaults.headers.common.Accept = 'application/json'
 Axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 Axios.defaults.withCredentials = true
 Axios.defaults.crossDomain = true
-// Axios.defaults.baseURL = 'http://ais.ipotekark.ru:3000'
-Axios.defaults.baseURL = 'http://localhost:3000'
+Axios.defaults.baseURL = 'http://ais.ipotekark.ru/'
+// Axios.defaults.baseURL = 'http://localhost:3000'
 
 // Bind Axios to Vue.
 Vue.$http = Axios
--- client/src/plugins/moment.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-
-moment.locale('ru')
-Vue.prototype.moment = moment
