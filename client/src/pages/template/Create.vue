1033d57569b8b9ec5eddce5ba1ab20cb467dddac
Alexandr Konuhov <alexandrkonuhov@gmail.com>
2019-02-13 11:39:23 +0300

    Add new file

---
+++ client/src/pages/template/Create.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <main-layout title="Добавить шаблон">
+    <b-card>
+      <b-form>
+        <b-form-row>
+          <b-form-group label="Наименование шаблона" class="col-12">
+            <b-input placeholder="Наименование шаблона" v-model="templateObject.name" />
+          </b-form-group>
+          <b-form-group label="Содержание шаблона" class="col-12">
+            <editor v-model="templateObject.text"></editor>
+          </b-form-group>
+        </b-form-row>
+        <b-btn variant="secondary" to="/templates">Назад</b-btn>
+        <b-btn variant="primary" class="ml-2">Создать</b-btn>
+      </b-form>
+    </b-card>
+  </main-layout>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+import MainLayout from '@/layout/MainLayout'
+export default {
+  name: 'PageDocumentCreate',
+  components: {
+    MainLayout,
+    Editor
+  },
+  data: () => ({
+    templateObject: {
+      name: null,
+      text: null
+    }
+  })
+}
+</script>
+
+<style lang="scss">
+</style>
